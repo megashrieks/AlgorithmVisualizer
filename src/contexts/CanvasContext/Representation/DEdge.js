ab1ee5c9554af00d3c44e560ebf38c2b57ed4f4d
megashrieks <shrikanthbudya@gmail.com>
2020-06-17 22:12:21 +0530

    arrow edge is implemented

--- src/contexts/CanvasContext/Representation/DEdge.js
+++ src/contexts/CanvasContext/Representation/DEdge.js
@@ -10,11 +10,30 @@ class DEdge{
     }
 
     get_border_position(start, end, radius) {
-        let angle = Math.atan2(start.x - end.x, start.y - end.y)+Math.PI;
+        let angle = Math.atan2(end.y - start.y, start.x - end.x)-Math.PI/2;
         return {
             x: start.x + Math.sin(angle) * radius,
             y: start.y + Math.cos(angle) * radius
         };
+    }
+
+    get_arrow_vertices(start, end) {
+        let angle = Math.atan2(end.y - start.y, start.x - end.x) - Math.PI / 2;
+        let offset_angle = Math.PI/4;
+        const length = 10;
+        let first_angle = angle - offset_angle * 3;
+        let second_angle = angle + offset_angle * 3;
+        return [
+            {
+                x: end.x + Math.sin(first_angle) * length,
+                y: end.y + Math.cos(first_angle) * length,
+            },
+
+            {
+                x: end.x + Math.sin(second_angle) * length,
+                y: end.y + Math.cos(second_angle) * length,
+            }
+        ]
     }
 
     draw() {
@@ -22,16 +41,26 @@ class DEdge{
         //TODO: detect the return false from the representation draw loop and handle it
         if (!this.start || !this.end) return false;
 
-        this.repr.context.lineWidth = 10;
+        let border_start = this.get_border_position(
+            this.start.position,
+            this.end.position,
+            this.start.effective_radius
+        );
+        let border_end = this.get_border_position(
+            this.end.position,
+            this.start.position,
+            this.end.effective_radius
+        );
 
-        let border_start = this.get_border_position(this.start.position,
-            this.end.position, this.start.effective_radius);
-        let border_end = this.get_border_position(this.end.position,
-            this.start.position, this.end.effective_radius);
-        this.repr.context.lineWidth = 5;
+        this.repr.context.lineWidth = 3;
         let temp = this.repr.context.lineCap;
         this.repr.context.lineCap = "round";
         this.repr.draw_line(border_start, border_end);
+        let [arrow1,arrow2] = this.get_arrow_vertices(border_start, border_end);
+        this.repr.draw_line(border_end, arrow1);
+        this.repr.draw_line(border_end, arrow2);
+
+
         this.repr.context.lineWidth = 1;
         this.repr.context.lineCap = temp;
     }
